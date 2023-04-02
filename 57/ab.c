3d48070cc3e8254fb1223fcf8356734541e3c80a
Arjun Vinod <arjunvinod09@gmail.com>
2023-04-02 22:41:46 +0530

    main no change

--- 57/ab.c
+++ 57/ab.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 void main(){
     printf("HI Hello this is 2nd change");
-    printf("from vyshnav 2");
-    printf("from arjun 2");
-    printf("vys loc");
-    printf("arj loc");
-    // trying lock 2
-    print("Time 10:40");
-    //type rmdir /s -rf .git inside testRepo-1 to remove a folder as repository from local
+    print("Time 10:40")
 }

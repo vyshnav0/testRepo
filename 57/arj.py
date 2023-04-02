e9a276fc80462f931061cdfba1d2358bb879a433
Arjun Vinod <arjunvinod09@gmail.com>
2023-04-02 17:41:32 +0530

    new fun

--- 57/arj.py
+++ 57/arj.py
@@ -1,2 +1,8 @@
 print("python better")
 print("No more restrictions on arjun branch")
+def thisIsDay2():
+    print("This fun created on April 2 Evening")
+    print("Should be studying IEFT instead")
+def main():
+    thisIsDay2()
+main()

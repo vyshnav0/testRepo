204bc740afe8c795dcb33cf9d16524db863f97ff
Arjun Vinod <arjunvinod09@gmail.com>
2023-04-03 06:17:30 +0530

    comment updated

--- 57/ab.c
+++ 57/ab.c
@@ -2,5 +2,5 @@
 void main(){
     printf("HI Hello this is 2nd change");
     print("Time 10:40")
-    //type rmdir /s -rf .git in testrepo-1 to remove directory as local repository
+    //type rmdir /s -rf .git in testrepo Or delete the .git folder(hidden) to remove directory as local repository
 }
--- 57/cd.py
+++ 57/cd.py
@@ -1,4 +1,4 @@
 def main():
     print("Function main created in main thread/cd.py")
 main()
-    #type rmdir /s -rf .git in testrepo-1 to remove directory as local repository
+    #type rmdir /s -rf .git in testrepo Or delete the .git folder(hidden) to remove directory as local repository

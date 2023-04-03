b7d7c7252f12abfea068db981fe2d314d74c0456
Arjun Vinod <arjunvinod09@gmail.com>
2023-04-03 06:18:20 +0530

    syntax

--- 57/ab.c
+++ 57/ab.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 void main(){
     printf("HI Hello this is 2nd change");
-    print("Time 10:40")
+    print("Time 10:40");
     //type rmdir /s -rf .git in testrepo Or delete the .git folder(hidden) to remove directory as local repository
 }
